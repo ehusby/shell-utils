39450f8c400ec46d4f21ddf9c46c11bf983d1eaf
husby036 <20846681+ehusby@users.noreply.github.com>
2022-03-30 14:18:57 -0500

    Update Windows exec batch scripts to reflect new starter scripts

--- windows/exec/osgeo-o4w.bat
+++ windows/exec/osgeo-o4w.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-set script_name=start_osgeo
+set script_name=start_osgeo_o4w-env_keep-path
 set icon_name=osgeo
 call %~dp0..\lib\create_starter_shortcut.vbs %script_name% %icon_name%
 start %~dp0..\shortcuts\%script_name%.lnk
